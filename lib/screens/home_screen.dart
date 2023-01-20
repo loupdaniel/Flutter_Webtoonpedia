7ec1318ebad971ca1c75b8c4f8cdbe6e8d172143
loupdaniel <loupdaniel12@gmail.com>
2023-01-21 01:30:09 +0900

    1st save(1:30)

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "Today's Webtoons",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
